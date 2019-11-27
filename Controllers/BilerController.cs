5ab0310335f7984420ab6f4635ebf39d5d9da040
christianfrom <43375010+christianfrom@users.noreply.github.com>
2019-11-27 12:00:17 +0100

    ModelLib og Controller tilføjet

--- Controllers/BilerController.cs
+++ Controllers/BilerController.cs
@@ -2,41 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Trafiki_ModelLib;
 
 namespace Trafiki_REST.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/biler")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class BilerController : ControllerBase
     {
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public static List<Bil> data = new List<Bil>
         {
-            return new string[] { "value1", "value2" };
+            new Bil(DateTime.Now, 1),
+        };
+
+        // GET: api/Biler
+        [HttpGet]
+        public IEnumerable<Bil> Get()
+        {
+            return data;
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        // GET: api/Biler/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST: api/Biler
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT: api/Biler/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {

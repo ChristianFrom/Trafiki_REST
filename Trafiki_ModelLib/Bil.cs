848dd37ae0faa32e865fd39594e3e1282c717a86
christianfrom <43375010+christianfrom@users.noreply.github.com>
2019-11-27 15:47:52 +0100

    Tilføjede ModelLib til solution

---
+++ Trafiki_ModelLib/Bil.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trafiki_ModelLib
+{
+    public class Bil
+    {
+        private DateTime _tidspunkt;
+        private int _nummer;
+
+        public Bil()
+        {
+            
+        }
+
+        public Bil(DateTime tidspunkt, int nummer)
+        {
+            _tidspunkt = tidspunkt;
+            _nummer = nummer;
+        }
+
+
+        public int Nummer
+        {
+            get => _nummer;
+            set => _nummer = value;
+        }
+
+        public DateTime Tidspunkt
+        {
+            get => _tidspunkt;
+            set => _tidspunkt = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Tidspunkt)}: {Tidspunkt}, {nameof(Nummer)}: {Nummer}";
+        }
+    }
+}
